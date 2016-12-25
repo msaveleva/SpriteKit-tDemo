143884fd0253366dc2a3c471401ec8c6270d0e2e
Maria Saveleva <mariiasaveleva@gmail.com>
2016-12-25 13:33:08 +0300

    WIP: second node

--- SpriteKittDemo/ScrollingBackground.swift
+++ SpriteKittDemo/ScrollingBackground.swift
@@ -18,20 +18,29 @@ class ScrollingBackground: SKSpriteNode {
     private var secondNode: SKSpriteNode?
 
     public func configureScrollingBackground() {
-        firstNode = SKSpriteNode(imageNamed: backgroundImageName)
+        firstNode = SKSpriteNode(color: .green, size: size)
+        secondNode = SKSpriteNode(color: .red, size: size)
 
-        guard let firstNode = self.firstNode else { return }
+        guard let firstNode = self.firstNode,
+            let secondNode = self.secondNode else { return }
 
         firstNode.anchorPoint = CGPoint.zero
-        firstNode.position = self.position
-        self.addChild(firstNode)
+        firstNode.position = CGPoint(x: -self.size.width/2, y: -self.size.height/2)
+        addChild(firstNode)
+
+        secondNode.anchorPoint = CGPoint.zero
+        secondNode.position = CGPoint(x: -self.size.width/2 + secondNode.size.width, y: -self.size.height/2)
+        addChild(secondNode)
     }
 
     public func update(currentTime: TimeInterval) {
-        //todo: apply for all child nodes
-        guard let firstNode = self.firstNode else { return }
+        guard let firstNode = self.firstNode,
+            let secondNode = self.secondNode else { return }
 
         firstNode.position = CGPoint(x: firstNode.position.x - self.velocity, y: firstNode.position.y)
+        secondNode.position = CGPoint(x: secondNode.position.x - self.velocity, y: secondNode.position.y)
+
+        //todo: add point check for firstNode
     }
 
 }
