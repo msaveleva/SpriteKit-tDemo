dd7a554e8dd7adb284dbca2cdcfe257db7dfaa05
Maria Saveleva <mariiasaveleva@gmail.com>
2016-12-27 20:21:33 +0300

    WIP: platforms

---
+++ SpriteKittDemo/Platform.swift
@@ -0,0 +1,14 @@
+//
+//  Platform.swift
+//  SpriteKittDemo
+//
+//  Created by MariaSaveleva on 27/12/2016.
+//  Copyright © 2016 MariaSaveleva. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Platform: SKShapeNode {
+    
+}
---
+++ SpriteKittDemo/PlatformsGenerator.swift
@@ -0,0 +1,28 @@
+//
+//  PlatformsGenerator.swift
+//  SpriteKittDemo
+//
+//  Created by MariaSaveleva on 27/12/2016.
+//  Copyright © 2016 MariaSaveleva. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class PlatformsGenerator {
+
+    private let kMaxPlatformsOnScreen = 2
+    private let kMaxPlatformWidth = 2
+    private let kMinPlatformWidth = 2
+    private let kMaxPlatformHeight = 2
+    private let kMinPlatformHeight = 2
+
+    private func generatePlatformsFor(node: SKNode) {
+        //generate platforms and place them on node
+    }
+
+    private func createPlatform() -> Platform? {
+        return nil
+    }
+
+}
