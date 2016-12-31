6fc0dad990b057b8a63b844e239b2c32c8b8b191
Maria Saveleva <mariiasaveleva@gmail.com>
2016-12-31 09:21:38 +0300

    WIP: files

---
+++ Mary's Adventure/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Mary's Adventure
+//
+//  Created by MariaSaveleva on 31/12/2016.
+//  Copyright © 2016 MariaSaveleva. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ Mary's Adventure/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  SpriteKittDemo
+//
+//  Created by MariaSaveleva on 31/12/2016.
+//  Copyright © 2016 MariaSaveleva. All rights reserved.
+//
+
+import Foundation
+
+let kPlatformHeight: CGFloat = 60.0
+let kCoinWidthHeight: CGFloat = 120
+
+let kPlayerCategory: UInt32 = 1
+let kIceCategory: UInt32 = 2
