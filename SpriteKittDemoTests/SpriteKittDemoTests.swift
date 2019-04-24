22325ccba28ea6b59d9861faf1db7a5f597e1dc8
Mariia Saveleva <mariiasaveleva@gmail.com>
2019-04-24 15:04:53 +0300

    added ci script and tests

---
+++ SpriteKittDemoTests/SpriteKittDemoTests.swift
@@ -0,0 +1,21 @@
+//
+//  SpriteKittDemoTests.swift
+//  SpriteKittDemoTests
+//
+//  Created by Mariia Saveleva on 23/04/2019.
+//  Copyright © 2019 MariaSaveleva. All rights reserved.
+//
+
+import XCTest
+
+class SpriteKittDemoTests: XCTestCase {
+    
+    func testTests() {
+        XCTAssertTrue(true) //Remove after testing Travis.
+    }
+    
+    func testTestsFailure() {
+        XCTAssertTrue(false) //Remove after testing Travis.
+    }
+
+}
